2699222226e2c3275bfcf76fede69efc8013c0ad
Nicolas Cena <nicolasnahuelcena@gmail.com>
2024-08-25 13:32:35 +0200

    [FEAT] Logo pestaña, añadimos acceso a mixins para todo el proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "src/styles/constants.scss";`,
+        additionalData: `
+          @import "src/styles/constants.scss";
+          @import "src/styles/mixins.scss";
+        `,
       },
     },
   },
-})
+});
