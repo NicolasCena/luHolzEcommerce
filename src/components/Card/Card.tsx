f1c2551473a044018d32b969281a95d0adbde125
Luciana <luciana.holzmann@gmail.com>
2024-08-27 03:22:11 +0200

    [FEAT] Contenido de los componentes Card, Footer y Navbar

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,24 +1,52 @@
-import styles from './Card.module.scss';
-import prueba from '../../assets/simulacionImg.webp';
+import styles from "./Card.module.scss";
+import prueba from "../../assets/simulacionImg.webp";
+import { useState } from "react";
 
 const Card = () => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <div className={styles['container-card']}>
-      <div className={styles['container-img-desc']}>
-        <div className={styles['container-img']}>
+    <div
+      className={`${styles["container-card"]} ${
+        isClicked ? styles["card-clicked"] : ""
+      }`}
+      onClick={handleClick}
+    >
+      <div className={styles["container-img-desc"]}>
+        <div className={styles["container-img"]}>
           <img src={prueba} alt="" />
         </div>
 
-        <div className={styles['container-descriprion-card']}>
-            <div className={styles['container-descriprion']}>
-                <p className={styles['text-product']}>ORGANIZADOR A4 MARIE TRANSPARENTE</p>
-                <p className={styles['price-product']}>$123,00</p>
-            </div>
-        </div>
+        <div
+          className={`${styles["container-descriprion-card"]} ${
+            isClicked ? styles["expanded"] : ""
+          }`}
+        >
+          <div className={styles["container-descriprion"]}>
+            <p className={styles["text-product"]}>
+              ORGANIZADOR A4 MARIE TRANSPARENTE
+            </p>
+            <p className={styles["price-product"]}>$123,00</p>
+          </div>
 
+          {isClicked && (
+            <div className={styles["container-buttons-card"]}>
+              <div className={styles["container-btns"]}>
+                <button className={styles["button-comprar"]}>COMPRAR</button>
+              </div>
+              <div className={styles["container-btns"]}>
+                <button className={styles["button-ver"]}>VER</button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export {Card}
+export { Card };
