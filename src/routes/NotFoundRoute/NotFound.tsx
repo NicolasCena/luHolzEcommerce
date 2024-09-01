333c804e506c3c5261f01adb08724366ba12d2ac
Luciana <luciana.holzmann@gmail.com>
2024-09-01 02:56:24 +0200

    [FEAT] Creación ruta NotFound y modificaciones en Template y App Routes

---
+++ src/routes/NotFoundRoute/NotFound.tsx
@@ -0,0 +1,23 @@
+import { Card } from "@components/Card/Card";
+import styles from "./NotFound.module.scss";
+
+const NotFound = () => {
+  return (
+    <div className={styles["container-notFound"]}>
+      <p className={styles["title-notFound"]}>PÁGINA NO ENCONTRADA</p>
+
+      <p className={styles["text-notFound"]}>
+        La página que estás buscando no existe.
+        <br />
+        Quizás te interesen los siguientes productos:
+      </p>
+
+      {/* //hacer map de 3 productos, card de ejemplo */}
+      <div>
+        <Card />
+      </div>
+    </div>
+  );
+};
+
+export { NotFound };
