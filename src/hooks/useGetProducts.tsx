b3c0e7d36db88bdd8cc1e5a8246de7206e93d7c0
Luciana <luciana.holzmann@gmail.com>
2024-09-05 16:12:55 +0200

    WIP on master

--- src/hooks/useGetProducts.tsx
+++ src/hooks/useGetProducts.tsx
@@ -1,44 +1,44 @@
-import { useState } from "react";
-import { app } from "../firebase";
-import { getDatabase, ref, get } from "firebase/database";
-import { ErrorDetails } from "firebase/vertexai-preview";
-import { ProductFirebase } from "../types/Product.type";
+// import { useState } from "react";
+// import { app } from "../firebase";
+// import { getDatabase, ref, get } from "firebase/database";
+// import { ErrorDetails } from "firebase/vertexai-preview";
+// import { ProductFirebase } from "../types/Product.type";
 
-export const useGetProducts = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<ErrorDetails | null>(null);
-  const [response, setResponse] = useState<ProductFirebase[]>([]);
+// export const useGetProducts = () => {
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState<ErrorDetails | null>(null);
+//   const [response, setResponse] = useState<ProductFirebase[]>([]);
 
-  const getProducts = async () => {
-    setLoading(true);
+//   const getProducts = async () => {
+//     setLoading(true);
 
-    try {
-      const db = getDatabase(app);
-      const productsRef = ref(db, "products");
-      const products = await get(productsRef);
+//     try {
+//       const db = getDatabase(app);
+//       const productsRef = ref(db, "products");
+//       const products = await get(productsRef);
 
-      if (products.exists()) {
-        const productArray: ProductFirebase[] = Object.entries(
-          products.val()
-        ).map(([, product]) => ({
-          ...(product as ProductFirebase),
-        }));
-        setResponse(productArray);
-      }
-    } catch (err) {
-      const firebaseError = err as ErrorDetails;
-      setError({
-        message: firebaseError.reason,
-      });
-    } finally {
-      setLoading(false);
-    }
-  };
+//       if (products.exists()) {
+//         const productArray: ProductFirebase[] = Object.entries(
+//           products.val()
+//         ).map(([, product]) => ({
+//           ...(product as ProductFirebase),
+//         }));
+//         setResponse(productArray);
+//       }
+//     } catch (err) {
+//       const firebaseError = err as ErrorDetails;
+//       setError({
+//         message: firebaseError.reason,
+//       });
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  return {
-    getProducts,
-    isLoadingGetProduct: loading,
-    errorGetProduct: error,
-    products: response,
-  };
-};
+//   return {
+//     getProducts,
+//     isLoadingGetProduct: loading,
+//     errorGetProduct: error,
+//     products: response,
+//   };
+// };
--- src/hooks/useSaveProduct.tsx
+++ src/hooks/useSaveProduct.tsx
@@ -1,50 +1,50 @@
-import { useState } from "react";
-import { app } from "../firebase";
-import { getDatabase, ref, set, push } from "firebase/database";
+// import { useState } from "react";
+// import { app } from "../firebase";
+// import { getDatabase, ref, set, push } from "firebase/database";
 
-interface Product {
-  name: string;
-  description: string;
-  isNew: boolean;
-  price: number;
-  images: string[];
-}
+// interface Product {
+//   name: string;
+//   description: string;
+//   isNew: boolean;
+//   price: number;
+//   images: string[];
+// }
 
-export const useSaveProduct = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<null | unknown>(null);
-  const [response, setResponse] = useState(false);
+// export const useSaveProduct = () => {
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState<null | unknown>(null);
+//   const [response, setResponse] = useState(false);
 
-  const sendProduct = async (product: Product) => {
-    setLoading(true);
+//   const sendProduct = async (product: Product) => {
+//     setLoading(true);
 
-    try {
-      const db = getDatabase(app);
-      const imageUrls: string[] = [];
-      const newDocRef = push(ref(db, "products"));
-      const productId = newDocRef.key;
+//     try {
+//       const db = getDatabase(app);
+//       const imageUrls: string[] = [];
+//       const newDocRef = push(ref(db, "products"));
+//       const productId = newDocRef.key;
 
-      await set(newDocRef, {
-        id: productId,
-        name: product.name,
-        description: product.description,
-        isNew: product.isNew,
-        price: product.price,
-        images: imageUrls,
-      });
+//       await set(newDocRef, {
+//         id: productId,
+//         name: product.name,
+//         description: product.description,
+//         isNew: product.isNew,
+//         price: product.price,
+//         images: imageUrls,
+//       });
 
-      setResponse(true);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+//       setResponse(true);
+//     } catch (error) {
+//       setError(error);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
 
-  return {
-    sendProduct,
-    isLoadingSendProduct: loading,
-    errorSendProduct: error,
-    responseSendProduct: response,
-  };
-};
+//   return {
+//     sendProduct,
+//     isLoadingSendProduct: loading,
+//     errorSendProduct: error,
+//     responseSendProduct: response,
+//   };
+// };
