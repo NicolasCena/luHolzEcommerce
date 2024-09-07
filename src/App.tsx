029582d4f33198b92821dbef0f10cb3a35a46b11
Luciana <luciana.holzmann@gmail.com>
2024-09-07 03:02:11 +0200

    [STYLE] Estilos y posicionamientos del navbar, iconos, menú y shop

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Footer } from "./components/Footer/Footer";
 import { AppRoutes } from "./routes/RoutesRoute/AppRoutes";
 import Header from "./components/Header/Header";
 import { useAuthState } from "./hooks/SignIn/useAuthState";
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export const App = () => {
   useAuthState();
